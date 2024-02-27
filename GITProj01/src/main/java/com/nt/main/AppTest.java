17347c9e424dd9378515e4c214aba087202b8d26
Nataraz <Nataraz@192.168.1.236>
2024-02-27 10:11:47 +0530

    task1 from dev1

--- GITProj01/src/main/java/com/nt/main/AppTest.java
+++ GITProj01/src/main/java/com/nt/main/AppTest.java
@@ -1,10 +1,14 @@
 package com.nt.main;
 
+import com.nt.service.ArithmeticOperations;
+
 public class AppTest{
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ArithmeticOperations ar=new ArithmeticOperations();
+		System.out.println("sum:"+ar.sum(100, 200));
+		System.out.println("sub:"+ar.sub(100, 200));
+		
 	}
 
 }
