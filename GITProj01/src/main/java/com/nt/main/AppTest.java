dea651126d5655d64482ef9ae1eb50babc58b57d
Nataraz <Nataraz@192.168.1.236>
2024-02-27 10:16:05 +0530

    task1 ---dev1

--- GITProj01/src/main/java/com/nt/main/AppTest.java
+++ GITProj01/src/main/java/com/nt/main/AppTest.java
@@ -8,7 +8,7 @@ public class AppTest{
 		ArithmeticOperations ar=new ArithmeticOperations();
 		System.out.println("sum:"+ar.sum(100, 200));
 		System.out.println("sub:"+ar.sub(100, 200));
-		
+		System.out.println("------------------");
 		
 	}
 
