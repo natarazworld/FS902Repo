7732a6b61228a4bd00d3d27a80ee6a5469f7de85
Nataraz <Nataraz@192.168.1.236>
2024-02-27 10:33:07 +0530

    TL --- coflict2

--- GITProj01/src/main/java/com/nt/main/AppTest.java
+++ GITProj01/src/main/java/com/nt/main/AppTest.java
@@ -10,7 +10,7 @@ public class AppTest{
 		System.out.println("sub:"+ar.sub(100, 200));
 		System.out.println("------------------");
 		System.out.println("------------------");
-		System.out.println("mul:"+ar.mul(100, 300)); //456
+		System.out.println("mul:"+ar.mul(100, 300)); //456 789
 		
 	}
 
