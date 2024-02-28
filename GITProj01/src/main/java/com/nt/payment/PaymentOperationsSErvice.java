90bdf304f198c81c7bf0b6b5ee5ae6ec6fc489c2
Nataraz <Nataraz@192.168.1.236>
2024-02-28 09:53:24 +0530

    DEv1 -B1 -task1

--- GITProj01/src/main/java/com/nt/payment/PaymentOperationsSErvice.java
+++ GITProj01/src/main/java/com/nt/payment/PaymentOperationsSErvice.java
@@ -1,5 +1,13 @@
 package com.nt.payment;
 
 public class PaymentOperationsSErvice {
+	
+	public  String  doCreditCardPayment() {
+		return "Credit card Payment is done";
+	}
+	
+	public  String doDebitCardPayment() {
+		return "Debit card payment is done";
+	}
 
 }
